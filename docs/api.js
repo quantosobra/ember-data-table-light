fe496d021dacc75b99ca1ec13e9805d993f3a6cd
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2016-06-22 09:00:08 -0300

    Released v0.0.1

---
+++ docs/api.js
@@ -0,0 +1,35 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CheckboxCell",
+        "CheckboxColumn",
+        "ColumnChooser",
+        "Configuration",
+        "DataTable",
+        "DataTableState",
+        "DataTableStateLocalStorage",
+        "DataTableStateSimpleAuth",
+        "EditableCell",
+        "EmberDataTableMixin",
+        "Empty",
+        "Footer",
+        "Header",
+        "Loading",
+        "Pagination",
+        "Search",
+        "TablePaginationMixin",
+        "TableSortingMixin"
+    ],
+    "modules": [
+        "ember-data-table-light_configuration"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-data-table-light/configuration",
+            "name": "ember-data-table-light_configuration",
+            "description": "Ember Data Table Light's configuration object.\n\nTo change any of these values, set them on the application's environment object, e.g.:\n\n```js\n// config/environment.js\nENV['ember-data-table-light'] = {\n  stateStorage: 'simple-auth'\n};\n```"
+        }
+    ],
+    "elements": []
+} };
+});
